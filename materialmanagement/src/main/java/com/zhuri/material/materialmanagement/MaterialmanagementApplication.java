29f7bf1adeb143f701a9258fbfe5d72da9c51aaf
cplayer <zhcplayer@gmail.com>
2018-07-13 22:23:44 +0800

    init commit

---
+++ materialmanagement/src/main/java/com/zhuri/material/materialmanagement/MaterialmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.zhuri.material.materialmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaterialmanagementApplication.class, args);
+    }
+}
---
+++ src/main/java/com/zhuri/material/MaterialApplication.java
@@ -0,0 +1,12 @@
+package com.zhuri.material;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaterialApplication.class, args);
+    }
+}
